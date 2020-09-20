a9276096545d1940be0d45455cd534f82c2d1756
Peter Thomson <dasbootface@gmail.com>
2020-09-20 12:57:36 +1200

    added pop up

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
 	$grid.imagesLoaded().progress( function() {
 		$grid.masonry('layout');
 	});
+
+	$('.grid-item').click(function(){
+		var image_data = $(this).data(image);
+		var image = image_data.image;
+		var description = `<p>${image.description}</p>`;
+		var title = `<h5 class="modal-title">${image.name}<i class="fa fa-times" class="close" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
+		var img = `<img src="${image.upload_location}" alt="${image.name}" />`;
+		$('#image-modal .modal-body').html(img + title + description);
+		$('.modal').modal('show');
+	});
 });
